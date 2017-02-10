e848b50f8d0e5a6431ca1f285376e97f7b0012a2
Twinone <twinonedevs@gmail.com>
2017-02-10 15:34:31 +0100

    Create skeleton

--- search/argp.py
+++ search/argp.py
@@ -5,40 +5,10 @@ import gui
 
 
 def _init_parser():
-    #Declare parser
-    parser = argparse.ArgumentParser(description='Get images from the web given a keyword')
-
-    #Optional arguments
-    parser.add_argument("-k", "--keyword", type=str, help='Word to find. Pex: "Cats"')
-    parser.add_argument("-n", "--num_results", type=int, help="Number of images to download")
-
-
-    parser.add_argument("-l", "--log", action="store_true", help="Activate loggin")
-    parser.add_argument("-c", "--console", action="store_true", help="Do not run UI")
-
-    try:
-        # Run the parser
-        return parser.parse_args()
-    except:
-        #if invalid data print help and exit
-        parser.print_help()
-        exit()
+    pass
 
 def parse():
-    #Get the data from the parser
-    args = _init_parser()
-
-    if not args.console:
-        gui.open_ui()
-        exit()
-
-    key = args.keyword
-    num = args.num_results
-
-    if not key: key = "kittens"
-    if not num: num = 5
-
-    search.do_search(key, num)
+    pass
 
 
 
--- search/search.py
+++ search/search.py
@@ -2,8 +2,19 @@ import os
 import re
 import urllib.request
 import webbrowser
+from os import listdir
+from os.path import isfile, join
+
 
 QUERY_URL = 'https://pixabay.com/en/photos/?q={0}&pagi={1}'
+
+HTML = """
+<html>
+<body>
+{0}
+</body>
+</html>
+"""
 
 
 def query(s, p):
@@ -18,73 +29,15 @@ def httpget(url):
 
 
 def getlinks_parser(s, n):
-    """
-    Dada una query devuelve los n links de las imagenes mas cercanas
-    Parser es menos eficiente que regex, pero siempre es correcto
-    """
-
-    p = 0
-    res = []
-    while len(res) < n:
-
-        p += 1
-        print("Getting page", n)
-        q = query(s, p)
-        data = str(httpget(q))
-
-        """
-        dom = lxml.html.fromstring(data)
-        res += [l for l in  dom.xpath('//img/@src')
-                if re.findall('https.*png', l)
-                or re.findall('https.*jpg', l)]
-        """
-        res += re.findall('<img.*?src="(https:\S*?\.jpg|https:\S*? \.png)".*?>', data)
-
-
-    return res[:n]
+    pass
 
 def save(key, l):
-    """Guarda las imagenes de una query"""
-    # 1. Crear carpeta llamada q
-    # 2. guardar las imagenes como q01.png/jpg
-    for i, x in enumerate(l):
-        name = key + str(i+1).zfill(3)
+   pass
 
 
-        if re.search("jpg", x):
-            name += ".jpg"
-        else:
-            name += ".png"
-
-        path = os.getcwd() + "/"+key+"/"
-        if not os.path.exists(path):
-            os.makedirs(path)
-        urllib.request.urlretrieve(x,  path + name)
-
-from os import listdir
-from os.path import isfile, join
-
-HTML = """
-<html>
-<body>
-{0}
-</body>
-</html>
-"""
 
 def open_html(key):
-    paths = ["./" + key + "/" + f for f in listdir(key) if isfile(join(key, f))]
-    content = ""
-    for p in paths:
-        content += """<img src="{0}" style="width:100%"></img><br><br> """.format(p)
-    html = HTML.format(content)
-    f = open("index.html", "w")
-    f.write(content)
-    f.close()
-    webbrowser.open(os.path.realpath("./index.html"))
+    pass
 
 def do_search(key, num):
-    res = getlinks_parser(key, num)
-    save(key, res)
-    open_html(key)
-
+    pass
